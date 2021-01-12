1f3a865aaed4ddac7996e2aae816a6f15658c76b
Hugo Meireles <meireles.hmm@gmail.com>
2021-01-12 23:58:25 +0000

    create product interface

---
+++ backend-nestjs/src/interfaces/IProduct.ts
@@ -0,0 +1,12 @@
+export interface IProduct {
+    _id: String;
+    name: String;
+    image: String;
+    description: String;
+    brand: String;
+    category: String;
+    price: Number;
+    countInStock: Number;
+    rating: Number;
+    numReviews: Number;
+}
