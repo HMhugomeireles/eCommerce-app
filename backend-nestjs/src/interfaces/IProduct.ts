138042deee92dce821746492e0ee2df1e2a267ab
Hugo Meireles <meireles.hmm@gmail.com>
2021-01-13 01:01:05 +0000

    update product interface

--- backend-nestjs/src/interfaces/IProduct.ts
+++ backend-nestjs/src/interfaces/IProduct.ts
@@ -1,3 +1,13 @@
+export interface IProductCategory {
+    _id: String;
+    category: String;
+}
+
+export interface IProductStock {
+    productId: String;
+    stockCount: Number;
+}
+
 export interface IProduct {
     _id: String;
     name: String;
