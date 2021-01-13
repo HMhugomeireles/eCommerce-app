86c4322ddf9f39697035fea948ab0dcea525f8a8
Hugo Meireles <meireles.hmm@gmail.com>
2021-01-13 00:10:00 +0000

    make the controller use mock

--- backend-nestjs/src/app.controller.ts
+++ backend-nestjs/src/app.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
+import { IProduct } from './interfaces/IProduct';
+
+import { MockProducts } from './mocks/ProductsMock'
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/api/product')
+  getProducts(): IProduct[] {
+    return MockProducts
   }
+
+  @Get('/api/product/:id')
+  getProductById(@Param('id') id: String): IProduct {
+    return MockProducts.find(item => item._id === id);
+  }
+
 }
